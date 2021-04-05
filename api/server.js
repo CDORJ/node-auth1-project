8ba52fbceee5a7564ae9e27fcbe6b1cf29873c82
sarahrosecooper <sarahrosecooperx@gmail.com>
2021-04-05 19:55:28 -0400

    auth-middleware.js: in progress

--- api/server.js
+++ api/server.js
@@ -11,7 +11,7 @@ const cors = require("cors");
   Users that do authenticate should have a session persisted on the server,
   and a cookie set on the client. The name of the cookie should be "chocolatechip".
 
-  The session can be persisted in memory (would not be adecuate for production)
+  The session can be persisted in memory (would not be adequate for production)
   or you can use a session store like `connect-session-knex`.
  */
 
@@ -25,7 +25,8 @@ server.get("/", (req, res) => {
   res.json({ api: "up" });
 });
 
-server.use((err, req, res, next) => { // eslint-disable-line
+server.use((err, req, res, next) => {
+  // eslint-disable-line
   res.status(500).json({
     message: err.message,
     stack: err.stack,
